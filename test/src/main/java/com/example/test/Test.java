c712c58a146d65903fba2616aacffefc1c1a69ea
fujaa <apriantofuja12@gmail.com>
2023-08-10 07:58:30 +0700

    Update Test.java

--- test/src/main/java/com/example/test/Test.java
+++ test/src/main/java/com/example/test/Test.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FinalProjectApplication {
+public class Test {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FinalProjectApplication.class, args);
+		SpringApplication.run(Test.class, args);
 	}
 
 }
